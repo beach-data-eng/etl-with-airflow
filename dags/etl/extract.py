3ab573453f0db5635baf46fc5ca3351aa1e1ddb9
beach-data-eng <beachratchata.k@gmail.com>
2026-01-12 11:01:16 +0700

    Update extract.py

--- dags/etl/extract.py
+++ dags/etl/extract.py
@@ -3,13 +3,30 @@ from google.cloud import storage
 import pandas as pd
 import io
 
+# https://storage.googleapis.com/beach-gcp-bucket/online_retail/raws/online_retail_sample.csv
+
+INPUT_BUCKET = "beach-gcp-bucket"
+INPUT_PATH = "online_retail/raws/online_retail_sample.csv"
+
+OUTPUT_BUCKET = "beach-gcp-bucket"
+OUTPUT_PATH = "online_retail/clean/online_retail_extract.csv"
+
+
 def extract_data():
     client = storage.Client()
-    bucket = client.bucket("beach-gcp-bucket")
-    blob = bucket.blob("online_retail/raws/online_retail_sample.csv")
 
+    # read input
+    blob = client.bucket(INPUT_BUCKET).blob(INPUT_PATH)
     data = blob.download_as_text()
     df = pd.read_csv(io.StringIO(data))
 
-    print("Extracted rows:", len(df))
-    return df
+    print(f"Extract rows: {len(df)}")
+
+    # write output
+    out_blob = client.bucket(OUTPUT_BUCKET).blob(OUTPUT_PATH)
+    out_blob.upload_from_string(
+        df.to_csv(index=False),
+        content_type="text/csv"
+    )
+
+    print(f"Extract saved to gs://{OUTPUT_BUCKET}/{OUTPUT_PATH}")
