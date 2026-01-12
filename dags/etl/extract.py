60c818df6b9c7d4516377416bfcf122dad55c61a
beach-data-eng <beachratchata.k@gmail.com>
2026-01-12 11:07:54 +0700

    Update extract.py

--- dags/etl/extract.py
+++ dags/etl/extract.py
@@ -5,28 +5,20 @@ import io
 
 # https://storage.googleapis.com/beach-gcp-bucket/online_retail/raws/online_retail_sample.csv
 
-INPUT_BUCKET = "beach-gcp-bucket"
-INPUT_PATH = "online_retail/raws/online_retail_sample.csv"
+BUCKET = "beach-gcp-bucket"
+INPUT_PATH = "online_retail/online_retail_sample.csv"
 
-OUTPUT_BUCKET = "beach-gcp-bucket"
-OUTPUT_PATH = "online_retail/clean/online_retail_extract.csv"
-
-
-def extract_data():
+def extract_data(run_dt: str):
+    
     client = storage.Client()
-
-    # read input
-    blob = client.bucket(INPUT_BUCKET).blob(INPUT_PATH)
-    data = blob.download_as_text()
+    input_blob = client.bucket(BUCKET).blob(INPUT_PATH)
+    data = input_blob.download_as_text()
     df = pd.read_csv(io.StringIO(data))
 
-    print(f"Extract rows: {len(df)}")
-
-    # write output
-    out_blob = client.bucket(OUTPUT_BUCKET).blob(OUTPUT_PATH)
-    out_blob.upload_from_string(
+    output_path = f"online_retail/raw/online_retail_extract_{run_dt}.csv"
+    client.bucket(BUCKET).blob(output_path).upload_from_string(
         df.to_csv(index=False),
-        content_type="text/csv"
+        content_type="text/csv",
     )
 
-    print(f"Extract saved to gs://{OUTPUT_BUCKET}/{OUTPUT_PATH}")
+    print(f"Extract saved: gs://{BUCKET}/{output_path}")
