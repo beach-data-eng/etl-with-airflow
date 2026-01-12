b8572740ffc54aa9dbc4956359eb6760b21f6cd5
beach-data-eng <beachratchata.k@gmail.com>
2026-01-12 11:13:39 +0700

    Update load.py

--- dags/etl/load.py
+++ dags/etl/load.py
@@ -1,11 +1,23 @@
 # dags/etl/load.py
 from google.cloud import bigquery
 
-def load_to_bq(df):
-    client = bigquery.Client()
-    table_id = "my_project.my_dataset.users"
+BUCKET = "beach-gcp-bucket"
+TABLE_ID = "stoked-summer-481406-b8.online_retail.sample_output"
 
-    job = client.load_table_from_dataframe(df, table_id)
+def load_to_bq(run_dt: str):
+    
+    client = bigquery.Client()
+
+    uri = f"online_retail/raw/online_retail_extract_{run_dt}.csv"
+
+    job_config = bigquery.LoadJobConfig(
+        source_format=bigquery.SourceFormat.CSV,
+        skip_leading_rows=1,
+        autodetect=True,
+        write_disposition="WRITE_TRUNCATE",
+    )
+
+    job = client.load_table_from_uri(uri, TABLE_ID, job_config=job_config)
     job.result()
 
-    print("Loaded to BigQuery")
+    print(f"Loaded {uri} → {TABLE_ID}")
